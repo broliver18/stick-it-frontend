f85841a90e5ef98265f3a044c33bdf98c83f84f1
broliver18 <bruno.olive504@gmail.com>
2023-06-29 17:21:19 -0400

    Make separate socket file

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
-import { io } from "socket.io-client";
 
 import Home from '../Home/Home';
 
-const socket = io("http://localhost:4000");
 const router = createBrowserRouter(createRoutesFromElements(
-  <Route path='/' element={ <Home/> }>
+  <Route path='/' element={ <Home/> } />
 
-  </Route>
 ));
 
 function App() {
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,38 +1,39 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { socket } from "../../socket";
 
-import './Home.css';
+import "./Home.css";
 
 function Home() {
-    const [nameInput, setNameInput] = useState('');
-    const [pinInput, setPinInput] = useState('');
+  const [nameInput, setNameInput] = useState("");
+  const [pinInput, setPinInput] = useState("");
 
-    const nameInputRef = useRef();
+  const nameInputRef = useRef();
 
-    const handleNameChange = e => setNameInput(e.target.value);
-    const handlePinChange = e => setPinInput(e.target.value);
+  useEffect(() => {
+    nameInputRef.current.focus();
+  }, []);
 
-    useEffect(() => {
-        nameInputRef.current.focus();
-    }, [])
+  const handleNameChange = (e) => setNameInput(e.target.value);
+  const handlePinChange = (e) => setPinInput(e.target.value);
+  const sendInfo = () => socket.emit('display-info', nameInput, pinInput);
 
-    return (
-        <div id="home">
-            <h1>Join a Game</h1>
-            <div className="game-input-info">
-                <p>Display Name</p>
-                <input 
-                    value={nameInput}
-                    onChange={handleNameChange}
-                    ref={nameInputRef}/>
-                <p>Game Pin</p>
-                <input 
-                    value={pinInput}
-                    onChange={handlePinChange}/>
-                <button>Join</button>
-            </div>
-        </div>
-    )
+  return (
+    <div id="home">
+      <h1>Join a Game</h1>
+      <div className="game-input-info">
+        <p>Display Name</p>
+        <input
+          value={nameInput}
+          onChange={handleNameChange}
+          ref={nameInputRef}
+        />
+        <p>Game Pin</p>
+        <input value={pinInput} onChange={handlePinChange} />
+        <button onClick={sendInfo}>Join</button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
