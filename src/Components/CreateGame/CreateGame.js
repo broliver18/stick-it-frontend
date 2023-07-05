8a04da11168e10bbcc6e1a7161a2c1b2f6686a25
broliver18 <bruno.olive504@gmail.com>
2023-07-05 18:44:36 -0400

    Add logic to add multiple questions

--- src/Components/CreateGame/CreateGame.js
+++ src/Components/CreateGame/CreateGame.js
@@ -5,12 +5,16 @@ import "./CreateGame.css";
 import QuestionForm from '../QuestionForm/QuestionForm'
 
 function CreateGame() {
+  const [numberOfQuestions, setNumberOfQuestions] = useState(1);
+
+  const addQuestion = () => setNumberOfQuestions((prevState) => prevState + 1);
+
   return (
     <div id="create-game">
       <h1>Quiz Creator</h1>
-      <div className="question-form"><QuestionForm/></div>
+      <div className="question-form">{Array(numberOfQuestions).fill(true).map((_, i) => <QuestionForm key={i} />)}</div>
       <div className="buttons">
-        <button>Add Question</button>
+        <button onClick={addQuestion}>Add Question</button>
         <button>Create Quiz</button>
       </div>
     </div>
