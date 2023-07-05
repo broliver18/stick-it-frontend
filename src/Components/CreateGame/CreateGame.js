43a89483c68b47a4f7ca7a0516c33afdac0ce5bc
broliver18 <bruno.olive504@gmail.com>
2023-07-05 16:02:35 -0400

    Finish styling for form

--- src/Components/CreateGame/CreateGame.js
+++ src/Components/CreateGame/CreateGame.js
@@ -6,7 +6,7 @@ import "./CreateGame.css";
 
 function CreateGame() {
   const [input, setInput] = useState({});
-  const [questionType, setQuestionType] = useState({ value: "short-answer" })
+  const [questionType, setQuestionType] = useState({ value: "short-answer" });
   const {
     question,
     answerOne,
@@ -17,7 +17,7 @@ function CreateGame() {
     shortAnswer,
   } = input;
 
-  const handleSelectChange = e => setQuestionType({ value: e.target.value })
+  const handleSelectChange = (e) => setQuestionType({ value: e.target.value });
 
   function handleInputChange(e) {
     const { name, value } = e.target;
@@ -28,39 +28,51 @@ function CreateGame() {
     if (questionType.value === "multiple-choice") {
       return (
         <>
-          <label>Answer 1</label>
-          <input
-            id="answer-one"
-            name="answerOne"
-            type="text"
-            value={answerOne}
-            onChange={handleInputChange}
-          />
-          <label>Answer 2</label>
-          <input
-            id="answer-two"
-            name="answerTwo"
-            type="text"
-            value={answerTwo}
-            onChange={handleInputChange}
-          />
-          <label>Answer 3</label>
-          <input
-            id="answer-three"
-            name="answerThree"
-            type="text"
-            value={answerThree}
-            onChange={handleInputChange}
-          />
-          <label>Answer 4</label>
-          <input
-            id="answer-four"
-            name="answerFour"
-            type="text"
-            value={answerFour}
-            onChange={handleInputChange}
-          />
-          <label>Correct Answer</label>
+          <div className="same-row">
+            <div className="column">
+              <label>Answer 1</label>
+              <input
+                id="answer-one"
+                name="answerOne"
+                type="text"
+                value={answerOne}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="column">
+              <label>Answer 2</label>
+              <input
+                id="answer-two"
+                name="answerTwo"
+                type="text"
+                value={answerTwo}
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+          <div className="same-row">
+            <div className="column">
+              <label>Answer 3</label>
+              <input
+                id="answer-three"
+                name="answerThree"
+                type="text"
+                value={answerThree}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="column">
+              <label>Answer 4</label>
+              <input
+                id="answer-four"
+                name="answerFour"
+                type="text"
+                value={answerFour}
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+          <label>Correct Answer (1-4)</label>
           <input
             id="correct-answer"
             name="correctAnswer"
@@ -90,7 +102,7 @@ function CreateGame() {
     return (
       <form>
         <label>Question Type</label>
-        <select onChange={handleSelectChange}>
+        <select id="question-type" onChange={handleSelectChange}>
           <option value="short-answer">Short Answer</option>
           <option value="multiple-choice">Multiple Choice</option>
         </select>
@@ -100,7 +112,7 @@ function CreateGame() {
           name="question"
           value={question}
           onChange={handleInputChange}
-        />  
+        />
         {renderAction()}
       </form>
     );
@@ -109,9 +121,7 @@ function CreateGame() {
   return (
     <div id="create-game">
       <h1>Quiz Creator</h1>
-      <div className="question-form">
-        {questionForm()}
-      </div>
+      <div className="question-form">{questionForm()}</div>
     </div>
   );
 }
