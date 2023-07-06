5e55337d62ff2bacde6662012ae28b0817532d36
broliver18 <bruno.olive504@gmail.com>
2023-07-06 10:35:03 -0400

    Properly position close button

--- src/Components/QuestionForm/QuestionForm.js
+++ src/Components/QuestionForm/QuestionForm.js
@@ -5,11 +5,30 @@ import { socket } from "../../socket";
 import "./QuestionForm.css";
 
 function CloseButton() {
-    return (
-        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Menu / Close_MD"> <path id="Vector" d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18" stroke="#3a5a40" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g> </g>
-        </svg>
-    )
+  return (
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <g id="Menu / Close_MD">
+          {" "}
+          <path
+            id="Vector"
+            d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18"
+            stroke="#3a5a40"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>{" "}
+        </g>{" "}
+      </g>
+    </svg>
+  );
 }
 
 function QuestionForm() {
@@ -116,7 +135,9 @@ function QuestionForm() {
 
   return (
     <div className="question-form">
-      <CloseButton/>
+      <div id="close-button">
+        <CloseButton />
+      </div>
       <form>
         <label>Question Type</label>
         <select id="question-type" onChange={handleSelectChange}>
