278c713fd9b3208604fa91794c2a7382f515737c
broliver18 <bruno.olive504@gmail.com>
2023-07-25 17:28:30 -0400

    Initiate flip animation

--- src/Components/PlayerGame/PlayerGame.js
+++ src/Components/PlayerGame/PlayerGame.js
@@ -5,21 +5,48 @@ import { socket } from "../../socket";
 import "./PlayerGame.css";
 
 function PlayerGame() {
-    const [searchParams] = useSearchParams();
-    const playerId = searchParams.get("id");
+  const [points, setPoints] = useState(new Array(24).fill(0));
+  const [isFlipped, setIsFlipped] = useState(false);
 
-    useEffect(() => {
-        socket.emit("player-join-game", playerId);
-        socket.on("test", () => console.log("it worked"));
+  const [searchParams] = useSearchParams();
+  const playerId = searchParams.get("id");
 
-        return () => socket.off("test", () => console.log("it worked"));
-    }, [])
+  useEffect(() => {
+    socket.emit("player-join-game", playerId);
+    socket.on("test", () => console.log("it worked"));
 
-    return (
-        <div id="player-game">
-            
-        </div>
-    )
+    return () => socket.off("test", () => console.log("it worked"));
+  }, []);
+
+  function flipCard() {
+    setIsFlipped(true);
+    setTimeout(() => {
+        setIsFlipped(false);
+    }, 1000);
+  };
+
+  return (
+    <div id="player-game" className="component-container-top">
+      <h1>Bruno's Quiz</h1>
+      <div class="grid-container">
+        {points.map((cardPoint, index) => {
+          return (
+            <div onClick={flipCard} className="flip-card" key={index}>
+              <div className={isFlipped ? "flip-card-inner flipped" : "flip-card-inner"}>
+                <div className="flip-card-front">
+                  <h4>Flip Me!</h4>
+                </div>
+                <div className="flip-card-back">
+                  <h4>{cardPoint}</h4>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div></div>
+    </div>
+  );
 }
 
 export default PlayerGame;
