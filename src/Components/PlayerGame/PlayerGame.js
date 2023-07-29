84bbaa08488cd4e7f8a578772f25411a336a4ef5
broliver18 <bruno.olive504@gmail.com>
2023-07-28 22:04:04 -0400

    Add answer messages

--- src/Components/PlayerGame/PlayerGame.js
+++ src/Components/PlayerGame/PlayerGame.js
@@ -12,6 +12,8 @@ function PlayerGame() {
   const [quizInfo, setQuizInfo] = useState({});
   const [input, setInput] = useState({ answer: "" });
   const [cardPoints, setCardPoints] = useState(new Array(24).fill(0));
+  const [isQuestionAnswered, setIsQuestionAnswered] = useState(true);
+  const [isCorrect, setIsCorrect] = useState(true);
   const [score, setScore] = useState(0);
   const [trigger, setTrigger] = useState(2);
 
@@ -54,37 +56,78 @@ function PlayerGame() {
     setInput((prevState) => ({ ...prevState, [name]: value }));
   }
 
-  function renderAction() {
-    if (questionInfo.questionType === "short-answer") {
-      return (
-        <div id="short-answer" className="component-container-top">
-          <h2>{questionInfo.question}</h2>
-          <input name="answer" value={input.answer} onChange={handleChange} />
-          <button>Answer Question</button>
-        </div>
-      );
+  function checkMultipleChoice(num) {
+    setIsQuestionAnswered(true);
+    if (num === questionInfo.correctAnswer) {
+      setIsCorrect(true);
     } else {
-      const { question, answerOne, answerTwo, answerThree, answerFour } =
-        questionInfo;
-      return (
-        <div id="multiple-choice" className="component-container-top">
-          <h2>{question}</h2>
-          <div className="multiple-choice-answers">
-            <div id="answer-one">
-              <h4>{answerOne}</h4>
-            </div>
-            <div id="answer-two">
-              <h4>{answerTwo}</h4>
-            </div>
-            <div id="answer-three">
-              <h4>{answerThree}</h4>
-            </div>
-            <div id="answer-four">
-              <h4>{answerFour}</h4>
+      setIsCorrect(false);
+      setTimeout(() => {
+        setIsQuestionAnswered(false);
+      }, 2000);
+    }
+  }
+
+  function checkShortAnswer() {
+    setIsQuestionAnswered(true);
+    if (input.answer === questionInfo.shortAnswer) {
+      setIsCorrect(true);
+    } else {
+      setIsCorrect(false);
+      setTimeout(() => {
+        setIsQuestionAnswered(false);
+      }, 2000);
+    }
+  }
+
+  function renderAction() {
+    if (isQuestionAnswered) {
+      if (isCorrect) {
+        return (
+          <div id="correct-message" className="message-container">
+            <h2>You answered correctly!</h2>
+            <h4>Flip a card to see how many points you'll get</h4>
+          </div>
+        );
+      } else {
+        return (
+          <div id="incorrect-message" className="message-container">
+            <h2>Sorry, that was incorrect!</h2>
+          </div>
+        );
+      }
+    } else {
+      if (questionInfo.questionType === "short-answer") {
+        return (
+          <div id="short-answer" className="component-container-top">
+            <h2>{questionInfo.question}</h2>
+            <input name="answer" value={input.answer} onChange={handleChange} />
+            <button>Answer Question</button>
+          </div>
+        );
+      } else {
+        const { question, answerOne, answerTwo, answerThree, answerFour } =
+          questionInfo;
+        return (
+          <div id="multiple-choice" className="component-container-top">
+            <h2>{question}</h2>
+            <div className="multiple-choice-answers">
+              <div id="answer-one">
+                <h4>{answerOne}</h4>
+              </div>
+              <div id="answer-two">
+                <h4>{answerTwo}</h4>
+              </div>
+              <div id="answer-three">
+                <h4>{answerThree}</h4>
+              </div>
+              <div id="answer-four">
+                <h4>{answerFour}</h4>
+              </div>
             </div>
           </div>
-        </div>
-      );
+        );
+      }
     }
   }
 
