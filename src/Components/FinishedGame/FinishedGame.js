23eb72d0d38300ef33c315e90656036a328eb281
broliver18 <bruno.olive504@gmail.com>
2023-07-31 15:26:59 -0400

    Add styling to FinishedGame

--- src/Components/FinishedGame/FinishedGame.js
+++ src/Components/FinishedGame/FinishedGame.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { socket } from "../../socket";
 
@@ -7,7 +7,15 @@ import "./FinishedGame.css";
 function FinishedGame() {
   return (
     <div id="finished-game" className="component-container-middle">
-      <h1>Congrats on finishing the game!</h1>
+      <div id="score-message" className="center">
+        <h1>Congrats on Finishing the Game!</h1>
+        <p>Don't leave this page until everyone finishes to see your ranking</p>
+        <div id="final-score" className="center">
+          <h2>Final Score:</h2>
+          <h1>125</h1>
+        </div>
+        <button>Home Page</button>
+      </div>
     </div>
   );
 }
