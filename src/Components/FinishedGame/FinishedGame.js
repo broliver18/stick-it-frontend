c43c6de8e0002c2fc09d04a83cb13e04a21d2003
broliver18 <bruno.olive504@gmail.com>
2023-07-31 22:40:44 -0400

    Finalize FinishedGame component

--- src/Components/FinishedGame/FinishedGame.js
+++ src/Components/FinishedGame/FinishedGame.js
@@ -1,10 +1,36 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { socket } from "../../socket";
 
 import "./FinishedGame.css";
 
 function FinishedGame() {
+  const [finalScore, setFinalScore] = useState();
+  const [trigger, setTrigger] = useState(0);
+
+  const [searchParams] = useSearchParams();
+  const playerId = searchParams.get("id");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getFinalScoreEvent = (score) => setFinalScore(score);
+
+    socket.emit("get-final-score", playerId);
+    socket.on("player-final-score", getFinalScoreEvent);
+
+    return () => socket.off("player-final-score", getFinalScoreEvent);
+  }, [playerId]);
+
+  useEffect(() => {
+    if (!trigger) return;
+    
+    socket.emit("end-game-player")
+    navigate("/");
+  }, [navigate, trigger]);
+
+  const incrementTrigger = () => setTrigger((prevState) => prevState + 1);
+
   return (
     <div id="finished-game" className="component-container-middle">
       <div id="score-message" className="center">
@@ -12,9 +38,9 @@ function FinishedGame() {
         <p>Don't leave this page until everyone finishes to see your ranking</p>
         <div id="final-score" className="center">
           <h2>Final Score:</h2>
-          <h1>125</h1>
+          <h1>{finalScore}</h1>
         </div>
-        <button>Home Page</button>
+        <button onClick={incrementTrigger}>Home Page</button>
       </div>
     </div>
   );
