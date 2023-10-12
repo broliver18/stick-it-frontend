4c24f9ce63bfe3d3a69b7ddb3b7e2af05fa2ba3d
broliver18 <bruno.olive504@gmail.com>
2023-10-12 17:53:47 -0400

    Revert back to previous commit

--- src/Components/ResetLink/ResetLink.js
+++ src/Components/ResetLink/ResetLink.js
@@ -108,7 +108,7 @@ function ResetLink() {
             )}
           </Formik>
           <Link className="standalone-link blue-label" to="/login">
-            Back to login page loser
+            Back to login page
           </Link>{" "}
         </div>
       );
