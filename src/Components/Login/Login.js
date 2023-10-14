c18a625c57cd7a31cd0e3b24689302cc56f79886
broliver18 <bruno.olive504@gmail.com>
2023-10-14 17:40:10 -0400

    Add rate limiter error to login component

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -83,6 +83,8 @@ function Login() {
                   localStorage.setItem("loggedIn", true);
                   localStorage.setItem("username", data.username);
                   navigate("/host");
+                } else {
+                  setError(data.status);
                 }
               });
           }}
