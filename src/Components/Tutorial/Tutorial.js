79a2cdf218c1d2d0d1f6660a8bed617fca8f3b2c
broliver18 <bruno.olive504@gmail.com>
2023-10-17 14:45:23 -0400

    Configure Tutorial component to right size

--- src/Components/Tutorial/Tutorial.js
+++ src/Components/Tutorial/Tutorial.js
@@ -1,16 +1,58 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
+import tutorialLogo from "../../images/tutorialLogo.png";
 
 import "./Tutorial.css";
 
 function Tutorial() {
+  const [tutorialNum, setTutorialNum] = useState(1);
+
+  const next = () => setTutorialNum((prevState) => prevState + 1);
+  const prev = () => setTutorialNum((prevState) => prevState - 1);
+
+  function renderAction() {
+    // eslint-disable-next-line default-case
+    switch (tutorialNum) {
+      case 1:
+        return (
+          <>
+            <h1>Welcome to Stick It!</h1>
+            <h2>
+              This short tutorial will walk you through how to create and play a
+              game.
+            </h2>
+            <h3>
+              If you want to dive right in, you can hit the "skip tutorial"
+              button and start playing. Otherwise press "next"!
+            </h3>
+            <div className="tutorial-img-container">
+              <img src={tutorialLogo} alt="tutorial logo" />
+            </div>
+          </>
+        );
+      case 2: 
+        return (
+          <>
+            
+          </>
+        )
+    }
+  }
+
   return (
     <div id="tutorial" className="container-top">
       <div className="instructions-container">
-        <h3 id="numbering">1/9</h3>
+        {renderAction()}
+        <h4 id="numbering">{tutorialNum}/9</h4>
+        <button id="skip">Skip Tutorial</button>
         <div className="nav-buttons">
-          <button id="prev">Previous</button>
-          <button id="next">Next</button>
+          <button onClick={prev} id="prev">
+            Previous
+          </button>
+          <button onClick={next} id="next">
+            Next
+          </button>
         </div>
       </div>
     </div>
